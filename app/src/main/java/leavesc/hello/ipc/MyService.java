c693767635cd5a5013ca46b4250daa76711f8642
sanbo <sanbo.xyz@gmail.com>
2022-10-24 14:36:22 +0800

    update sth. at 2022-10-24 14:36:22

--- app/src/main/java/leavesc/hello/ipc/MyService.java
+++ app/src/main/java/leavesc/hello/ipc/MyService.java
@@ -3,6 +3,7 @@ package leavesc.hello.ipc;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * 作者：leavesC
@@ -12,6 +13,19 @@ import android.os.IBinder;
 public class MyService extends Service {
 
     public MyService() {
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+//        String stringField, int intField, boolean booleanField
+        Demo demo =new Demo("what", 99,true);
+        Log.i("sanbo",demo.toString());
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
